19fb0130d8292aa2e3a00db0534005f20b56979d
jsharma <jsharma@GTLT02414>
2019-08-07 09:25:03 +1000

    adding more test cases

--- parenttest/apptype1/test_appconf.py
+++ parenttest/apptype1/test_appconf.py
@@ -5,6 +5,14 @@ class TestStringMethods(unittest.TestCase):
    def setUp(self):
       self.config = ConfigObj('parent/apptype1/appname1/app.conf')
 
+   def test_is_visible_exists(self):      
+      self.is_visible_value= self.config['report']['is_visible']
+      self.assertIsNotNone(self.is_visible_value)
+
+   def test_auth_required_exists(self):      
+      self.auth_required_value= self.config['report']['auth_required']
+      self.assertIsNotNone(self.auth_required_value)
+
    def test_is_visible(self):      
       self.is_visible_value= self.config['report']['is_visible']
       self.assertEqual(self.is_visible_value, 'true')
