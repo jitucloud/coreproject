fb98847f98514f6991e513775652646c218b62f9
jsharma <jsharma@GTLT02414>
2019-08-06 16:40:03 +1000

    adding comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# coreproject
+### start jenkins in docker with local volume
+> docker run -d -p 8080:8080 -p 50000:50000 -v c:/workspace/personal/code/coreprojectvolume:/var/jenkins_home jenkins/jenkins
+
+### run apptype1 unit test cases
+
+> python -m unittest discover -s parenttest/apptype1
--- parenttest/apptype1/test_appconf.py
+++ parenttest/apptype1/test_appconf.py
@@ -3,7 +3,7 @@ from configobj import ConfigObj
 
 class TestStringMethods(unittest.TestCase):    
    def setUp(self):
-      self.config = ConfigObj('../../parent/apptype1/appname1/app.conf')
+      self.config = ConfigObj('parent/apptype1/appname1/app.conf')
 
    def test_is_visible(self):      
       self.is_visible_value= self.config['report']['is_visible']
